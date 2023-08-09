0c933238c558a70f3776cc2d6c6890fe24ab08d1
estevanmendes <estevan.amazonas@gmail.com>
2023-08-09 12:05:55 -0300

    methods added to avoid repetition

--- client.py
+++ client.py
@@ -4,43 +4,51 @@ import time
         
 
 class TestServerMethods(unittest.TestCase):
+    
+    def check_status_code(self,response) -> None:
+        self.assertEqual(200,response.status_code,"Error in the deafult endpoint, status code inst 200")
+    
+    def check_message_display(self,response) -> None:
+        self.assertGreater(len(response.text),1,'No message is being displayed')
+
+
 
     def test_default(self) -> None:
         print(url)
         response=requests.get(url)
-        self.assertEqual(response.status_code,200,"Error in the deafult endpoint, status code inst 200")
-        self.assertGreater(len(response.text),1,'No message is being displayed')
+        self.check_status_code(response)
+        self.check_message_display(response)
         time.sleep(1)
 
     def test_file_creation(self) -> None:
         for file in ['d1','d2','d3']:
             response=requests.post(url+f'file_handler/{file}')
-            self.assertEqual(response.status_code,200,"Error in the deafult endpoint, status code inst 200") 
-            self.assertGreater(len(response.text),1,'The file content is not being returned')
+            self.check_status_code(response)
+            self.check_message_display(response)
             print(response.text)
             time.sleep(.2)
 
     def test_file_update(self) -> None:
         for file in ['d1','d2','d3']:
             response=requests.put(url+f'file_handler/{file}')
-            self.assertEqual(response.status_code,200,"Error in the deafult endpoint, status code inst 200") 
-            print(response.text)
+            self.check_status_code(response)
+            self.check_message_display(response)
             time.sleep(.2)
 
 
     def test_file_get(self) -> None:
         for file in ['d1','d2','d3']:
             response=requests.get(url+f'file_handler/{file}')
-            self.assertEqual(response.status_code,200,"Error in the deafult endpoint, status code inst 200")
-            self.assertGreater(len(response.text),1,'The file content is not being returned') 
-            print(response.text) 
+            self.check_status_code(response)
+            self.check_message_display(response)
             time.sleep(0.2)
     
     def test_file_remotion(self) -> None:
         for file in ['d1','d2','d3']:
             response=requests.delete(url+f'file_handler/{file}')
-            self.assertEqual(response.status_code,200,"Error in the deafult endpoint, status code inst 200")
-            self.assertGreater(len(response.text),1,'The file content is not being returned') 
+            self.check_status_code(response)
+            self.check_message_display(response)
+            self.assertIn(file,response.text,'The file was not removed') 
             time.sleep(.2)
 
 
