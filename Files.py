da2b2bd19c8b913e13347a485141ebd580aec725
estevanmendes <estevan.amazonas@gmail.com>
2023-08-09 12:05:35 -0300

    decorator added to avoid repetition

--- Files.py
+++ Files.py
@@ -14,8 +14,37 @@ class Files:
     @staticmethod
     def erase_file(path:str) -> None:
         os.remove(path)
-     
 
+    def check_filename_in(list_of_files:list,filename:str) -> bool:
+        if filename in list_of_files:
+            return True
+        else:
+            return False    
+    
+    def error_messages(type):
+        """
+        Decorator for returning proper error messages
+        type:["overwriting","nonexistent"]
+        """
+        def _message(method):
+            def wrapper(self,*args,**kwargs):
+                if kwargs.get('filename'):
+                    filename=kwargs['filename']
+                else:
+                    filename=args[0]      
+
+                filenameIn=Files.check_filename_in(self.names,filename)                
+                if type=="overwriting" and filenameIn:                                     
+                    return'The filename already exist, please delete it before creating a new'   
+                             
+                elif type=='nonexistent' and not filenameIn:
+                    return 'The filename does not exist, please create it before.'   
+                             
+                else:
+                    return method(self,*args,**kwargs)
+                
+            return wrapper         
+        return _message
 
     def __init__(self,root_path:str,extension:str='.txt') -> None:
         """
@@ -26,47 +55,44 @@ class Files:
         self.names=[]
         self.paths={}
 
-    def check_filename_in(self,filename:str) -> bool:
-        if filename in self.names:
-            return True
-        else:
-            return False
 
-    def get_path(self,filename:str) -> str:
-        return os.path.join(self.root_path,filename+self.extension)  
-
-    def add_file(self,filename:str) -> str:
-        if self.check_filename_in(filename):
-            return'The filename already exist, please delete it before creating a new'
-          
+    @error_messages('overwriting')
+    def add_file(self,filename:str) -> str:      
         self.names.append(filename)
         path=self.get_path(filename)
         self.paths[filename]=path
         return Files.create_file(path)
 
-
+    @error_messages('nonexistent')
     def remove_file(self,filename:str) -> str:
-        if not self.check_filename_in(filename):
-            return 'The filename does not exist, please create it before.'
-        
         Files.erase_file(self.paths[filename])
         self.names.remove(filename)
         del self.paths[filename]
-        return '{filename} deleted'
+        return f'{filename} deleted'
     
+    @error_messages('nonexistent')
     def load_file(self,filename) -> str:
-        if not self.check_filename_in(filename):
-            return 'The filename does not exist, please create it before.'
-        
         with open(self.paths[filename],'r') as f:
             data=f.read()
         return data
-
+    
+    @error_messages('nonexistent')
     def edit(self,filename:str) -> str:
-        if not self.check_filename_in(filename):
-            return 'The filename does not exist, please create it before.'
         Files.erase_file(self.paths[filename])
         return Files.create_file(self.paths[filename])
 
+ 
+
+    def get_path(self,filename:str) -> str:
+        return os.path.join(self.root_path,filename+self.extension)  
+
     def __str__(self) -> str:
         return f"{len(self.names)} Files with extension {self.extension}"
+
+
+if __name__=="__main__":
+    ExampleFiles=Files(root_path='sample_folder',extension='.txt')
+    print(ExampleFiles.add_file(filename='someFile'))
+    print(ExampleFiles.edit(filename='someFile'))
+    print(ExampleFiles.remove_file(filename='someFile'))
+    print(ExampleFiles.remove_file(filename='someFile'))
